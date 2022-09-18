8b6b891aa11b37db1e4cd01119c7be0c32797ad5
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-09-18 12:10:24 +0900

    feat(db): add iana header fields and listable info

---
+++ _tools/generate.ts
@@ -0,0 +1,27 @@
+import { parse } from "../deps.ts";
+import meta from "../meta.json" assert { type: "json" };
+
+const ISNA_SOURCE =
+  "https://www.iana.org/assignments/http-fields/field-names.csv";
+const res = await fetch(ISNA_SOURCE);
+const rawCsv = await res.text();
+const csvMap = parse(rawCsv, {
+  skipFirstRow: true,
+});
+
+const list = csvMap.map((fields) => fields["Field Name"] as string).map((
+  value,
+) => value.toLowerCase());
+
+interface Fields {
+  listable?: boolean;
+  forbidden?: boolean;
+}
+
+const obj = list.reduce((acc, cur) => {
+  acc[cur] = meta[cur as keyof typeof meta] ?? {};
+
+  return acc;
+}, {} as Record<string, Fields>);
+
+Deno.writeTextFile("./db.json", JSON.stringify(obj, undefined, 2));
